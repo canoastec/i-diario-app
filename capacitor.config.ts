cf5a8cad69ef5cda07913d5b519fde2ef9ad1ae0
Francis Boardman <alunomarlonbueno@outlook.com>
2025-11-19 16:28:46 -0300

    fix: alteração de branding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.ionicframework.appoffline153013',
-  appName: 'Portábilis Diário',
+  appName: 'Canoastec Diário',
   webDir: 'www',
   server: {
     androidScheme: 'https',
