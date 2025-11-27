eaeee99bd84034cad517a9740ccb251cee464875
Francis Boardman <alunomarlonbueno@outlook.com>
2025-11-27 16:46:13 -0300

    fix: update app ID and name in Capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ionicframework.appoffline153013',
+  appId: 'br.gov.rs.canoastec.idiario',
   appName: 'Canoastec Diário',
   webDir: 'www',
   server: {
